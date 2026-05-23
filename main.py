b156dddb9adf18edfc93bb8f37a5f9b4bdc85d30
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-05-23 13:18:16 +0545

    2 random agents fight

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from player import Human, Agent
 from game import Game
 
 
-human_player = Human(code=1)
-agent = Agent(code=-1)
-game = Game(human_player, agent)
-game.match()
-game.draw()
+# human_player = Human(code=1)
+agent1 = Agent(code=1)
+agent2 = Agent(code=-1)
+game = Game(agent1, agent2)
+for i in range(50):
+	game.match()
+# game.draw()
+print(f"Agent 1: {agent1.score}")
+print(f"Agent 2: {agent2.score}")
