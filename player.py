f9420fa33dfe95987058d3ec6cac3812bfad1f06
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-05-23 11:46:15 +0545

    an agent that plays aimlessly via the initialized weights and biases (9,2,9)

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+from numpy import argmax
 from network import Network
 # Acts as a base class / interface for all kinds of players (agents, human)
 class Player:
@@ -20,8 +21,38 @@ class Human(Player):
 class Agent(Player):
 	def __init__(self, code):
 		super().__init__(code)
-		# self.network = Network()
+		# 9 inputs for each cell having (-1,0,1)
+		# 9 outputs as probabilities
+		self.network = Network(9,2,9)
 	
 	def move(self, grid):
 		# move logic
-		return (0,0)
+		# convert grid to 1D vector
+		inp = grid.reshape(1,9)
+		probabilities = self.network.forward(inp).reshape(3,3)
+		# mask the probabilities for not giving illegal moves
+		for i in range(3):
+			for j in range(3):
+				# set non clear cells probability to 0 or -inf
+				if grid[i][j] != 0:
+					probabilities[i][j] = 0 # -inf for non sigmoid outputs
+		# probability matrix
+		# print(probabilities)
+		# getting the highest probability index
+		index = argmax(probabilities) # returns flat array index
+		(x,y) = (index//3, index%3) # convert flat index to 2D
+		# print(index)
+		# print(f"p[{x}][{y}] = {probabilities[x][y]}")
+		return (x,y)
+
+if __name__ == "__main__":
+	import numpy as np
+	from random import choice
+	agent = Agent(code=-1)
+	grid = np.zeros((3,3), dtype=int)
+	for i in range(grid.shape[0]):
+			for j in range(grid.shape[1]):
+				grid[i][j] = choice([0,1,-1])
+	
+	print(grid)
+	agent.move(grid)
