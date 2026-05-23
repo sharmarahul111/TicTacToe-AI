1d3167d479babf7594b75534260d0bac24dda772
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-05-23 12:13:32 +0545

    better gameplay loop with check game over condition

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ class Game:
 		self.grid = np.zeros((3,3), dtype=int)
 		self.player_A = player_A
 		self.player_B = player_B
+		self.turn = True # True = Player A, False = Player B
 	
 	def randomize(self):
 		for i in range(self.grid.shape[0]):
@@ -27,24 +28,35 @@ class Game:
 			print()
 			print("-"*13)
 
-	def play(self, player):
+	def play(self):
+		# turn = True for player A, False for player B
+		player = self.player_A
+		if not self.turn:
+			player = self.player_B
+		self.turn = not self.turn # change the turn for next round
 		# it is expected that the players return valid move
 		(x,y) = player.move(self.grid)
 		self.grid[x][y] = player.code
 		# return (x,y)
 
-	def match(self):
+	def check_game_over(self):
+		game_over = False
+		# check if the cells are filled
+		if np.count_nonzero(self.grid) == 9:
+			game_over = True
+			return game_over
+		# check for win/lose condition
+		
+
+
+		return game_over
+
+
+	def match(self, mode="silent"):
 		# this method handles the complete start to finish of the match logic
 		# logic for game completion and assigning wins/losses
-		for i in range(9):
-			self.play(self.player_A)
-			self.play(self.player_B)
+		while not self.check_game_over():
+			self.play()
 			# for visual purpose
-			# (x,y) = self.play(self.player_A)
-			# print(f"Player A: {x}, {y}")
-			# self.draw()
-			# print()
-			# (x,y) = self.play(self.player_B)
-			# print(f"Player B: {x}, {y}")
-			# self.draw()
-			# print("--------------------")
+			if mode != "silent":
+				self.draw()
