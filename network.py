fdff8416a206d6ceca1da0f390f638db6a86ce41
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-05-23 08:31:14 +0545

    initial network setup

---
+++ network.py
@@ -0,0 +1,54 @@
+import numpy as np
+class Layer:
+	def __init__(self, weights=None, biases=None):
+		self.weights = weights
+		self.biases = biases
+
+class Network():
+	def __init__(self, *args: int):
+		self.design = args
+		self.layers = []
+		for i in range(1,len(args)):
+			weights = np.random.randn(args[i-1], args[i])*.5
+			biases = np.zeros(args[i])
+			self.layers.append(Layer(weights, biases))
+	
+	def __str__(self):
+		s = "\n["
+		i=1
+		for layer in self.layers:
+			s+= f"\n\tlayer[{i}]: "
+			# weights
+			s+= "\n\t\tWeights ["
+			for l in layer.weights:
+				s+= "\n\t\t\t"
+				s+= str(l)
+				s+= " "
+			s+= "\n\t\t]"
+
+			# biases
+			s+= "\n\t\tBiases ["
+			for l in layer.biases:
+				s+= "\n\t\t\t"
+				s+= str(l)
+				s+= " "
+			s+= "\n\t\t]"
+
+			i+=1
+		s+= "\n]\n"
+		return s
+
+	def sigmoid(self, x):
+		return 1./(1+np.exp(-x))
+
+	def tanh(self, x):
+		return np.tanh(x)
+
+	def forward(self, inp):
+		activation = inp
+		for i in range(len(self.layers)-1):
+			activation = activation @ self.layers[i].weights + self.layers[i].biases
+			activation = self.tanh(activation)
+		activation = activation @ self.layers[-1].weights + self.layers[-1].biases
+		activation = self.sigmoid(activation)
+		return activation
