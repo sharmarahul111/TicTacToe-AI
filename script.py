f0ad56c033a37cc362520c05451d89d64e694962
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-05-24 20:44:24 +0545

    Bruteforce never loses!!!

--- script.py
+++ script.py
@@ -1,6 +1,12 @@
 from game import Game
-from player import Human, Algorithm
+from player import *
 game = Game()
 
 algo = Algorithm()
-game.match(algo, algo, draw_board=True, print_result=True)
+random = RandomPlayer()
+bruteforce = Bruteforce()
+for i in range(20):
+	game.match(bruteforce, random, draw_board=False, print_result=True)
+	game.match(random, bruteforce, draw_board=False, print_result=True)
+	game.match(bruteforce, algo, draw_board=False, print_result=True)
+	game.match(algo, bruteforce, draw_board=False, print_result=True)
